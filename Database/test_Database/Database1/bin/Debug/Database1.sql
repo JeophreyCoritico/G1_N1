e6525a0f1211e93f507302f3884630a0b4c01371
atzin-em <37525882+atzin-em@users.noreply.github.com>
2019-10-29 09:13:06 +1100

    Added Sql Files for Working Together

--- Database/test_Database/Database1/bin/Debug/Database1.sql
+++ Database/test_Database/Database1/bin/Debug/Database1.sql
@@ -15,8 +15,8 @@ SET NUMERIC_ROUNDABORT OFF;
 GO
 :setvar DatabaseName "Database1"
 :setvar DefaultFilePrefix "Database1"
-:setvar DefaultDataPath "C:\Users\102571922\AppData\Local\Microsoft\VisualStudio\SSDT\test_Database"
-:setvar DefaultLogPath "C:\Users\102571922\AppData\Local\Microsoft\VisualStudio\SSDT\test_Database"
+:setvar DefaultDataPath "C:\Users\atzin\AppData\Local\Microsoft\VisualStudio\SSDT\test_Database"
+:setvar DefaultLogPath "C:\Users\atzin\AppData\Local\Microsoft\VisualStudio\SSDT\test_Database"
 
 GO
 :on error exit
@@ -38,6 +38,80 @@ IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
 GO
 USE [$(DatabaseName)];
 
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                CURSOR_DEFAULT LOCAL 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET PAGE_VERIFY NONE,
+                DISABLE_BROKER 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET TARGET_RECOVERY_TIME = 0 SECONDS 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET QUERY_STORE (QUERY_CAPTURE_MODE = AUTO, OPERATION_MODE = READ_WRITE) 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+PRINT N'Creating [dbo].[Table_Test]...';
+
+
+GO
+CREATE TABLE [dbo].[Table_Test] (
+    [Id]    INT        NOT NULL,
+    [Tests] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+DELETE FROM Table_Test;
+INSERT INTO Table_Test (Id, Tests) VALUES
+(0,'Hello'),
+(1,'Bye'),
+(2,'Nope'),
+(3,'Yes')
+GO
 
 GO
 PRINT N'Update complete.';
---
+++ Database/test_Database/Database1/obj/Debug/postdeploy.sql
@@ -0,0 +1,18 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+DELETE FROM Table_Test;
+INSERT INTO Table_Test (Id, Tests) VALUES
+(0,'Hello'),
+(1,'Bye'),
+(2,'Nope'),
+(3,'Yes')
+GO
